3e0e9cc0f48a6a154c97bcb91671d1e9822965fb
greysky10 <alan777lee@gmail.com>
2020-06-17 19:05:07 -0400

    construct user object

--- userProfile/UserProfileView.js
+++ userProfile/UserProfileView.js
@@ -1,16 +1,34 @@
 import {questions} from userProfile\UserProfileView.js
+let users =[];
 let userName = '';
 let userId=null;
 let userAnwsers=[];
-let userAnwsers=[];
+let userQeustions=[];
+
+class user{
+    constructor(userName, email, displayname, password, tags, photo, isadmin, isFlagged){
+        this.userName = userName;
+        this.email = email;
+        this.displayname = displayname;
+        this.password = password;
+        this.tags = tags;
+        this.photo = photo;
+        this.isadmin = isadmin;
+        this.isFlagged = isFlagged;
+    }
+}
+
 function getAllQeustions(user){
     for(var i=0; i<userTags.length; i++)
     {
         var currQeustion = userQeustion[i];
         var Ttable = document.getElementById('questionTable');
         var row = table.insertRow(-1);
-        newTag.innerHTML=`$currTag`;
+        newTag.innerHTML=`${user}`;
     }
+}
+fucntion uploadPhoto(e){
+    var newP = document.getElementById(importform);
 }
 function getAllTags(user){
     var userTags = user.tags;
@@ -25,7 +43,7 @@ function getAllTags(user){
         var Ttable = document.getElementById('tagsTable');
         var row = table.insertRow(-1);
         var newTag = row.inserCell(fit);
-        newTag.innerHTML=`$currTag`;
+        newTag.innerHTML=`${currTag}`;
     }
 
 
