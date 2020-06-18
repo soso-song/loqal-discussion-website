7c6640582e22faf84fc19f94e2b6da9b8a4d0f72
greysky10 <alan777lee@gmail.com>
2020-06-18 18:29:57 -0400

    update the useredit fom

--- userProfile/EditProfile.js
+++ userProfile/EditProfile.js
@@ -1,6 +1,8 @@
 let user;
-const userEditForm = document.querySelector('#editForm');
+const userEditForm = document.querySelector('#editForm').value;
+console.log(userEditForm);
 userEditForm.addEventListener('submit',edit);
+
 function load(e){
     document.querySelector('#userName').innerHTML=user.Name;
     document.querySelector('newPassword').innerHTML=user.password;
@@ -8,9 +10,13 @@ function load(e){
 
 
 function edit(e){
+    e.preventDefault();
     const newUserName= document.querySelector('userName').value;
 	const newPassword = document.querySelector('#newBookAuthor').value;
     const newConfirmPassword = document.querySelector('#confirmPassword').value;
     const newPhoto = document.querySelector('#photoscr').value;
+    user.Name=newUserName;
+    user.password=newPassword;
+    user.photosrc=newPhoto;
     console.log(newUserName);
 }
