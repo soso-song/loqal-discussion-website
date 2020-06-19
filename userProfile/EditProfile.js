a27d0a026e8f7fe3b2df0f2d81be3604f95a86f7
greysky10 <alan777lee@gmail.com>
2020-06-19 14:46:24 -0400

    finish the basic function of edit user

--- userProfile/EditProfile.js
+++ userProfile/EditProfile.js
@@ -1,20 +1,27 @@
 const user = users[2];
 
 const userEditForm = document.querySelector('#editForm');
-console.log(user.username);
 userEditForm.addEventListener('submit',edit);
 
-document.querySelector('#userName').defaultValue=user.username;
-document.querySelector('#newPassword').defaultValue=user.password;
 
 function edit(e){
     e.preventDefault();
     const newUserName= document.querySelector('#userName').value;
 	const newPassword = document.querySelector('#newPassword').value;
     const newConfirmPassword = document.querySelector('#confirmPassword').value;
-    const newPhoto = document.querySelector('#photoscr').value;
-    user.Name=newUserName;
-    user.password=newPassword;
-    user.photosrc=newPhoto;
-    console.log(newUserName);
+    const newPhoto = document.querySelector('#photoSrc').value;
+    if (newUserName !== null){
+        user.Name=newUserName;
+    }
+    if (newPassword == newConfirmPassword){
+        user.password=newPassword;
+    } else{
+        window.alert("You passward and confirm password is not same")
+    }
+    if (newPhoto!==null)
+    {
+        user.photosrc=newPhoto;
+    }
+
+    console.log(user.Name)
 }
