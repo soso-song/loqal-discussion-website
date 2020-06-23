c574fd2b359b7b1010b3b45b4d9c9a79aed00805
greysky10 <alan777lee@gmail.com>
2020-06-22 20:48:43 -0400

    cp the brad's html to userfolder

--- user/userprofile.js
+++ user/userprofile.js
@@ -17,3 +17,106 @@ $(document).ready(function() {
         $('#following').removeClass("hideme");
 	});
 });
+
+const pageUser = users[2];
+curr_user = users[0];
+getAllQeustionsNum(pageuser)
+function getAllQeustionsNum(user){
+    var res =0;
+    for(var i=0; i<num_questions; i++)
+    {
+        if (question[i].user_id=page_user.id)
+        {
+            res++;
+        }
+    }
+    var numUser = document.querySelector('#userheading');
+    numUser.innerHTML = `Questions (${res})`;
+}
+
+function getAllQeustions(user){
+    for(var i=0; i<num_questions; i++)
+    {
+        var currQeustion = questions[i];
+        var newQ = document.querySelector("#listcontainter")
+        newQ.innerHTML += `        <div class="shortquestion">
+        <a class="squestion" href="../answer/answer.html">Where can I buy some avocados?</a>
+        <div class="sinfo">Asked by <a href="#">Knower</a> - June 19, 2020 - 5 Answers - Unresolved</div>
+    </div>`
+
+    }
+}
+
+function uploadPhoto(e){
+    var newP = document.getElementById('importForm');
+    var newPsrc = newP.datafile.value;
+}
+
+function following(){
+    var status = document.getElementById("follow");
+    if (status.innerHTML === "follow") {
+        curr_user.following.push(pageUser);
+        pageUser.followed.push(curr_user);
+        status.innerHTML = "following";
+      } else 
+      {
+        status.innerHTML = "follow";
+        curr_user.following.pop();
+        pageUser.followed.pop();
+      }
+}
+getAllTags(pageUser);
+function getAllTags(user){
+    var userTags = user.tag_list;
+    for(var i=0; i<userTags.length; i++)
+    {
+        var currTag = userTags[i];
+        var fit=-1;
+        if(tags[currTag].is_geo==true)
+        {
+            fit=0;
+        }
+        var Ttable = document.getElementById('tagsTable');
+        var row = Ttable.insertRow(-1);
+        var newTag = row.insertCell(fit);   
+        newTag.innerHTML=`${tags[currTag].name}`;
+    }
+}
+listAllquestions(pageUser);
+function listAllquestions(user){
+    var question;
+    for (question of questions)
+    {
+        if (question.user_id == user.user_id)
+        {
+            var newQ = document.getElementById("questions").innerHTML=question.is_solved();
+            var newR = newQ.insertRow(-1);
+            var newStatus = newR.insertCell(0);
+            var newContent = newR.insertCell(1);
+            newStatus.innerHTML = `${question.content}`;
+            newContent.innerHTML = `${question.is_solved}`;
+        }
+    }
+}
+var shown = 0;
+pageUser.follower = [users[0],users[1],users[2]];
+function loadFollowInfo(e){
+    var fInfo = document.getElementById("followerinfo");
+    if (shown == 0){
+        fInfo.style.visibility = "visible";
+        shown = 1;
+        var follower;
+        for (follower of pageUser.follower)
+        {
+            var newFo = document.getElementById("followerinfotable");
+            var row = newFo.insertRow(-1);
+            var f = row.insertCell(0);
+            f.innerHTML = `${follower.username}`;
+        }
+    } else{
+        fInfo.style.visibility = "hidden";
+        var newFo = document.getElementById("followerinfotable");
+        newFo.innerHTML = `<th>Followers</th>`;
+        shown = 0;
+    }
+}
--- userProfile/UserDashBoard.js
+++ userProfile/UserDashBoard.js
@@ -1,36 +1,3 @@
-currUser = users[0];
+$(document).ready(function() {
 
-const tagsOption = document.querySelector("#tags");
-const userEditForm = document.querySelector('#askQuestion');
-userEditForm.addEventListener('submit',AskQuestion);
-loadTags();
-function loadTags(e)
-{
-    for (var i=0; i<tags.length; i++)
-    {
-        var newTagName =  tags[i].name;
-        var newT = document.createElement('option');
-        newT.value=newTagName;
-        newT.text=newTagName;
-        tagsOption.appendChild(newT);
-    }
-}
-
-function AskQuestion(e)
-{
-    e.preventDefault();
-    var newQuestion;
-    
-}
-function loadQ(e)
-{
-    var Q;
-    window.alert("hi");
-    for(Q of questions)
-    {
-        if (Q.user_id = currUser.user_id)
-        {
-            console.log(Q);
-        }
-    }
-}
+});
