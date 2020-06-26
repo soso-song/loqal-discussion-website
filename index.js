d0e6807e9c24cf6e5dfa5a44db311ab61e020660
burtonrider <behrad88@gmail.com>
2020-06-26 19:50:51 -0400

    Adding search bar and updating nav bar

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$('#searchForm').submit(function(e) {
+    e.preventDefault();
+
+    const searchinput = $('#searchinput').val();
+
+	window.location.href = `../searchQuestion/search_question.html?search_key=${searchinput}`;
+
+  });
+
+});
