6abcc4d7e39ec180b31f2b5adec697bb1b7398f1
burtonrider <behrad88@gmail.com>
2020-06-30 13:04:49 -0400

    Cleaning index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict"
+
 $(document).ready(function() {
 
 	$('#searchForm').submit(function(e) {
@@ -5,8 +7,7 @@ $(document).ready(function() {
 
     const searchinput = $('#searchinput').val();
 
-	window.location.href = `../searchQuestion/search_question.html?search_key=${searchinput}`;
-
+	  window.location.href = `../searchQuestion/search_question.html?search_key=${searchinput}`;
   });
 
 });
