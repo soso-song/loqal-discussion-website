32cfc530a35bda57fd164e13bde2cfcfefad0ceb
niconicolii <nicoletut.wang@mail.utoronto.ca>
2020-08-10 14:16:40 -0400

    moved getTagList function to sharedTag.js

--- pub/sharedJs/sharedTag.js
+++ pub/sharedJs/sharedTag.js
@@ -58,64 +58,35 @@ async function updateTag(id,name){
 	return newURL;
 }
 
-// get the user's username and displayname from server
-// async function getUserInfo(user_id){
-// 	const url = '/users/' + user_id;
 
-// 	const request = new Request(url, {
-// 		method: 'get',
-// 		headers: {
-// 			'Accept': 'application/json, text/plain, */*',
-// 			'Content-Type': 'application/json'
-// 		}
-// 	});
-// 	let userInfo;
-// 	await fetch(request)
-// 	.then((res) => {
-// 		if (res.status === 200) {
-//            	// return a promise that resolves with the JSON body
-//            	return res.json();
-//        	} else {
-//             // alert('Could not get user.');
-//        	} 
-// 	})
-// 	.then((json) => {
-// 		userInfo = {
-//            	displayname: json.displayname,
-//            	username: json.username
-//         };
-// 	})
-// 	.catch((error) => {
-// 		console.log(error)
-// 	})
-// 	return userInfo;
-// }
+// Loads all the tags this user is following to
+async function getTagList(tags){
+	let mytags = '';
+    const url = '/tagIdToName';
+    const data = {
+        ids: tags
+    }
+    const request = new Request(url, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+    });
 
-// // get the user's username and displayname from server
-// async function getAnswer(answer_id){
-// 	const url = '/answers/' + answer_id;
-
-// 	const request = new Request(url, {
-// 		method: 'get',
-// 		headers: {
-// 			'Accept': 'application/json, text/plain, */*',
-// 			'Content-Type': 'application/json'
-// 		}
-// 	});
-// 	let answer;
-// 	await fetch(request)
-// 	.then((res) => {
-// 		if (res.status === 200) {
-//            	return res.json();
-//        	} else {
-//             alert('Could not get answer.');
-//        	} 
-// 	})
-// 	.then((json) => {
-// 		answer = json.answer;
-// 	})
-// 	.catch((error) => {
-// 		console.log(error)
-// 	})
-// 	return answer;
-// }
+    await fetch(request)
+    .then((res) => {
+        return res.json();
+    })
+    .then((json) => {
+        let newDiv = '';
+        for(let i=0; i < json.length; i++){
+            mytags+=`<span class="tag">${json[i]}</span>`;
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    return mytags;
+}
