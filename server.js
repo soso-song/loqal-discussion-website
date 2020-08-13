bbe27677c363abc00cf830a04814215af18c32ab
burtonrider <behrad88@gmail.com>
2020-08-13 14:40:14 -0400

    Adding welcome screen

--- server.js
+++ server.js
@@ -11,16 +11,6 @@ const { mongoose } = require('./db/mongoose')
 mongoose.set('bufferCommands', false);
 mongoose.set('useFindAndModify', false);
 
-// import the mongoose models
-const { User, Question, Notice, Tag, Report} = require('./models/loqal')
-
-// to validate object IDs
-const { ObjectID } = require('mongodb')
-
-/*** Helper functions below **********************************/
-function isMongoError(error) { // checks for first error returned by promise rejection if Mongo database suddently disconnects
-	return typeof error === 'object' && error !== null && error.name === "MongoNetworkError"
-}
 
 // body-parser: middleware for parsing HTTP JSON body into a usable object
 const bodyParser = require('body-parser') 
@@ -40,18 +30,6 @@ const sessionChecker = (req, res, next) => {
     }    
 };
 
-// middleware for mongo connection error for routes that need it
-const mongoChecker = (req, res, next) => {
-	// check mongoose connection established.
-	if (mongoose.connection.readyState != 1) {
-		log('Issue with mongoose connection')
-		res.status(500).send('Internal server error')
-		return;
-	} else {
-		next()	
-	}	
-}
-
 /*** Session handling **************************************/
 // Create a session cookie
 app.use(session({
@@ -63,43 +41,6 @@ app.use(session({
         httpOnly: true
     }
 }));
-
-// Middleware for authentication of resources
-const authenticate = (req, res, next) => {
-	if (req.session.user) {
-		User.findById(req.session.user).then((user) => {
-			if (!user) {
-				return Promise.reject()
-			} else {
-				req.user = user
-				next()
-			}
-		}).catch((error) => {
-			res.status(401).send("Unauthorized")
-		})
-	} else {
-		res.status(401).send("Unauthorized")
-	}
-}
-
-
-// Middleware for authentication of resources
-const adminAuthenticate = (req, res, next) => {
-	if (req.session.user) {
-		User.findById(req.session.user).then((user) => {
-			if (!user || !user.isAdmin) {
-				return Promise.reject()
-			} else {
-				req.user = user
-				next()
-			}
-		}).catch((error) => {
-			res.status(401).send("Unauthorized")
-		})
-	} else {
-		res.status(401).send("Unauthorized")
-	}
-}
 
 const users = require('./routes/users')
 app.use('/users', users)
@@ -130,14 +71,13 @@ app.use('/notice', notice)
 
 // route for root: should redirect to login route
 app.get('/', sessionChecker, (req, res) => {
-	res.redirect('/login')
+	//res.redirect('/login')
+	res.sendFile(path.join(__dirname, '/pub/index.html'));
 })
 
 // login route serves the login page
 app.get('/login', sessionChecker, (req, res) => {
 	res.sendFile(path.join(__dirname, '/pub/register/register.html'))
-	//render the handlebars template for the login page
-	//res.render('login.hbs');
 })
 
 // dashboard route will check if the user is logged in and server
@@ -145,10 +85,6 @@ app.get('/login', sessionChecker, (req, res) => {
 app.get('/dashboard', (req, res) => {
 	if (req.session.user) {
 		res.sendFile(path.join(__dirname, '/pub/user/user_dashboard.html'))
-		// render the handlebars template with the email of the user
-		//res.render('dashboard.hbs', {
-		//	email: req.session.email
-		//})
 	} else {
 		res.redirect('/login')
 	}
